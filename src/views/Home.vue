7a51002b46db17ae11bde31700a2957751e0e0b3
tavoGeek <gustavoc.mx@gmail.com>
2020-12-08 21:42:19 -0600

    PRUEBA GET

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,11 +9,30 @@
 import Mapa from "../components/Mapa";
 import SeccionPrincipal from "../components/SeccionPrincipal";
 
+import { db } from "../common/Firebase";
+
 export default {
   name: "Home",
+  mounted() {
+    console.log(this.$route.query.id);
+    this.saveData();
+  },
   components: {
     Mapa,
     SeccionPrincipal,
   },
+  methods: {
+    saveData() {
+      const data = {
+        prueba: this.$route.query.id
+      };
+
+      db.collection("pruebas")
+        .add(data)
+        .then(() => {
+          console.log("Guardado con éxito!");
+        });
+    }
+  }
 };
 </script>
