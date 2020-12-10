9080a71abc705fc3d68b3963db112ae1e2ef5367
tavoGeek <gustavoc.mx@gmail.com>
2020-12-10 10:27:20 -0600

    VIDEO LOGIN

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,36 +1,46 @@
 <template>
-  <v-container>
-    <v-row class="login d-flex justify-center align-center">
-      <v-col cols="6">
-        <v-card>
-          <v-card-title> Login </v-card-title>
-          <v-card-text>
-            <v-text-field
-              outlined
-              dense
-              label="Usuario"
-              placeholder="Ingresa tu usuario"
-              v-model="user.user"
-            ></v-text-field>
+  <div>
+    <video autoplay="autoplay" loop="loop" muted="muted" class="video">
+      <source src="../assets/bus-login.mp4" type="video/mp4" />
+    </video>
+    <v-container>
+      <v-row class="login d-flex justify-center align-center">
+        <v-col cols="6">
+          <v-card>
+            <v-card-title class="d-flex justify-center">
+              <v-img src="../assets/logo.png" max-width="50%"></v-img>
+            </v-card-title>
+            <h1 class="text-center">Login</h1>
+            <v-card-text>
+              <v-text-field
+                outlined
+                dense
+                label="Usuario"
+                placeholder="Ingresa tu usuario"
+                v-model="user.user"
+              ></v-text-field>
 
-            <v-text-field
-              outlined
-              dense
-              label="Contraseña"
-              placeholder="Ingresa tu contraseña"
-              type="password"
-              v-model="user.psw"
-            ></v-text-field>
+              <v-text-field
+                outlined
+                dense
+                label="Contraseña"
+                placeholder="Ingresa tu contraseña"
+                type="password"
+                v-model="user.psw"
+              ></v-text-field>
 
-            <v-alert dense outlined type="error" v-if="error">
-              {{ errorMessage }}
-            </v-alert>
-            <v-btn block @click="iniciarSesion">Ingresar</v-btn>
-          </v-card-text>
-        </v-card>
-      </v-col>
-    </v-row>
-  </v-container>
+              <v-alert dense outlined type="error" v-if="error">
+                {{ errorMessage }}
+              </v-alert>
+              <v-btn block @click="iniciarSesion" color="primary"
+                >Ingresar</v-btn
+              >
+            </v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
 </template>
 
 <script>
@@ -78,5 +88,12 @@ export default {
 .login {
   height: 85vh;
   color: red;
+}
+
+.video {
+  position: absolute;
+  max-width: 100vw;
+  // max-width: 300%;
+  // width: 100%;
 }
 </style>
